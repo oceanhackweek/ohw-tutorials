2a202a91993b7181dd8cc40a00abdaadabfdc878
Default Jupyter user <aimee@developmentseed.org>
2021-08-02 22:00:04 +0000

    Updated Overview location

--- data-viz/tutorial/load_data.py
+++ data-viz/tutorial/load_data.py
@@ -5,7 +5,7 @@ import erddapy
 # https://coastwatch.pfeg.noaa.gov/erddap/tabledap/cwwcNDBCMet.nc?station%2Clongitude%2Clatitude%2Ctime%2Cwspd%2Cgst%2Cwvht&time%3E=2021-07-01T00%3A00%3A00Z&time%3C=2021-08-01T22%3A35%3A00Z
 default_constraints = {
     "time>=": "2021-01-01T00:00:00Z",
-    "time<=": "2021-03-01T00:00:00Z",        
+    "time<=": "2021-08-01T00:00:00Z",        
     "wvht>=": 1
 }
 def load_data(
